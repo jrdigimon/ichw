1563ff015867937c8588d27da07e641763e0dce4
jrdigimon <43393872+jrdigimon@users.noreply.github.com>
2018-12-03 21:21:27 +0800

    Update currency.py

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -8,19 +8,25 @@ def main():
         '''Returns: Substring of s; up to, but not including, the first space
            Parameter s: the string to slice
            Precondition: s has at least one space in it'''
-        t = s.split()
+        t = s.split(' ',maxsplit = 1)
         return t[0]
     def test_before_space():
+        '''Test the before_space function.'''
         assert(before_space('0.8963 Euros') == '0.8963')
+        assert(before_space(' 0.6983 Euros') == '')
+        assert(before_space('ab cd') == 'ab')
     test_before_space()
     def after_space(s):
         '''Returns: Substring of s after the first space
            Parameter s: the string to slice
            Precondition: s has at least one space in it'''
-        t = s.split()
+        t = s.split(' ',maxsplit = 1)
         return t[1]
     def test_after_space():
+        '''Test the after_space function'''
         assert(after_space('0.8963 Euros') == 'Euros')
+        assert(after_space('0.8963 Euros is convertable') == 'Euros is convertable')
+        assert(after_space(' Can this pass?') == 'Can this pass?')
     test_after_space()
     def first_inside_quotes(s):
         '''Returns: The first substring of s between two (double) quote characters
@@ -29,7 +35,10 @@ def main():
         t = s.split('"')
         return t[1]
     def test_first_inside_quotes():
+        '''Test the first_inside_quotes function.'''
         assert(first_inside_quotes('A "B C" D') == 'B C')
+        assert(first_inside_quotes('{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}') == 'from')
+        assert(first_inside_quotes('This assignment is "interesting"') == 'interesting')
     test_first_inside_quotes()
     def get_from(json):
         '''Returns: The FROM value in the response to a currency query.
@@ -40,8 +49,13 @@ def main():
         re = first_inside_quotes(use)
         return re
     def test_get_from():
-        json = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
-        assert(get_from(json) == '2 United States Dollars')
+        '''Test the get_from function.'''
+        json1 = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
+        assert(get_from(json1) == '2 United States Dollars')
+        json2 =  '{"from":"","to":"","success":false,"error":"Source currency code is invalid."}'
+        assert(get_from(json2) == '')
+        json3 = '{ "from" : "2.5 Chinese Yuan", "to" : "0.31507457567753 Euros", "success" : true, "error" : "" }'
+        assert(get_from(json3) == '2.5 Chinese Yuan')
     test_get_from()
     def get_to(json):
         '''Returns: The TO value in the response to a currency query.
@@ -52,8 +66,13 @@ def main():
         re = first_inside_quotes(use)
         return re
     def test_get_to():
-        json = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
-        assert(get_to(json) ==  '1.825936 Euros')
+        '''Test the get_to function.'''
+        json1 = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
+        assert(get_to(json1) ==  '1.825936 Euros')
+        json2 = '{"from":"","to":"","success":false,"error":"Source currency code is invalid."}'
+        assert(get_to(json2) == '')
+        json3 = '{ "from" : "2.5 Chinese Yuan", "to" : "0.31507457567753 Euros", "success" : true, "error" : "" }'
+        assert(get_to(json3) == '0.31507457567753 Euros')
     test_get_to()
     def has_error(json):
         '''Returns: True if the query has an error; False otherwise.
@@ -66,10 +85,12 @@ def main():
         else:
             return True
     def test_has_error():
-        json =  '{"from":"","to":"","success":false,"error":"Source currency code is invalid."}'
-        assert(has_error(json) == True)
-        json = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
-        assert(has_error(json) == False)
+        json1 =  '{"from":"","to":"","success":false,"error":"Source currency code is invalid."}'
+        assert(has_error(json1) == True)
+        json2 = '{"from":"2 United States Dollars","to":"1.825936 Euros","success":true,"error":""}'
+        assert(has_error(json2) == False)
+        json3 = '{ "from" : "", "to" : "", "success" : false, "error" : "Currency amount is invalid." }'
+        assert(has_error(json3) == True)
     test_has_error()
     def currency_response(currency_from, currency_to, amount_from):
         '''Returns: a JSON string that is a response to a currency query.
@@ -82,13 +103,17 @@ def main():
            Parameter amount_from: amount of currency to convert
            Precondition: amount_from is a float'''
         from urllib.request import urlopen
-        doc = urlopen('http://cs1110.cs.cornell.edu/2016fa/a1server.php?from={}&to={}&amt={}'.format(currency_from,currency_to,amout_from))
+        doc = urlopen('http://cs1110.cs.cornell.edu/2016fa/a1server.php?from={}&to={}&amt={}'.format(currency_from,currency_to,amount_from))
         docstr = doc.read()
         doc.close()
         json = docstr.decode('ascii')
         return json
     def test_currency_response():
-        assert(currency_response(USD,EUR,2.5) =={ "from" : "2.5 United States Dollars", "to" : "2.1589225 Euros", "success" : true, "error" : "" })
+        '''Test the currency_response function.'''
+        assert(currency_response('USD','EUR',2.5) == '{ "from" : "2.5 United States Dollars", "to" : "2.1589225 Euros", "success" : true, "error" : "" }')
+        assert(currency_response('usd','EUR',2.5) == '{ "from" : "", "to" : "", "success" : false, "error" : "Source currency code is invalid." }')
+        assert(currency_response('USD','EUR','joking') == '{ "from" : "", "to" : "", "success" : false, "error" : "Currency amount is invalid." }')
+    test_currency_response()
     def iscurrency(currency):
         '''Returns: True if currency is a valid currency. False otherwise.
            Parameter currency: the currency code to verify
@@ -115,8 +140,11 @@ def main():
         else:
             return False
     def test_iscurrency():
-        assert(iscurrency('CHY') == True)
+        '''Test the iscurrency function'''
+        assert(iscurrency('CNY') == True)
         assert(iscurrency('China Yuan') == False)
+        assert(iscurrency('cny') == False)
+    test_iscurrency()
     def exchange(currency_from, currency_to, amount_from):
         '''Returns: amount of currency received in the given exchange.
            Parameter currency_from: the currency on hand
@@ -125,13 +153,26 @@ def main():
            Precondition: currency_to is a string for a valid currency code
            Parameter amount_from: amount of currency to convert
            Precondition: amount_from is a float'''
-        text = currency_response(currency_from, currency_to, amount_from)
-        temp1 = get_to(text)
-        temp2 = temp1.split()
-        amount_to = float(temp2[0])
-        return amount_to
+        if iscurrency(currency_from) is True and iscurrency(currency_to) is True and amount_from.isdigit() is True:
+            text = currency_response(currency_from, currency_to, amount_from)
+            temp1 = get_to(text)
+            temp2 = temp1.split()
+            amount_to = float(temp2[0])
+            return amount_to
+        elif iscurrency(currency_from) is False:
+            amount_to = 'Source currency code is invalid.'
+            return amount_to
+        elif iscurrency(currency_to) is False:
+            amount_to = 'Exchange currency code is invalid.'
+            return amount_to
+        else:
+            amount_to = 'Currency amount is invalid.'
+            return amount_to
     def test_exchange():
-        assert(exchange(USD,EUR,2.5) == 2.1589225)
-    print('passed')
+        assert(exchange('USD','EUR',2.5) == 2.1589225)
+        assert(exchange('usd','EUR',2.5) == 'Source currency code is invalid.')
+        assert(exchange('USD','eur',2.5) == 'Exchange currency code is invalid.')
+        assert(exchange('USD','EUR','joking') == 'Currency amount is invalid.')
+    print('All tests passed.')
 if __name__ == "__main__":
     main()
